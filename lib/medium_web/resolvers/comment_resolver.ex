d7430576db109c65167a089909e4abd103d6656c
Husssein Omar <husseinomar6190@gmail.com>
2023-01-18 16:29:10 +0300

    add: delete comment

--- lib/medium_web/resolvers/comment_resolver.ex
+++ lib/medium_web/resolvers/comment_resolver.ex
@@ -28,6 +28,19 @@ defmodule MediumWeb.Resolvers.CommentResolver do
     end
   end
 
+  def delete_post(_, %{input: input}, %{context: %{current_user: current_user}}) do
+    with  {:ok, comment}  <- Comments.get_comment(input.id),
+          {:ok, _} <- verfy_user(current_user.id, comment.user_id),
+          {:ok, %Comment{}} <- Comments.delete_comment(comment) do
+
+          {:ok, true}
+       else
+        {:error, :not_found} -> {:error, "Comment not found"}
+        {:error, :not_allowed} -> {:error, "You are not allowed to delete this comment"}
+        {:error, %Ecto.Changeset{} = changeset} -> {:error, Utils.format_changeset_error(changeset)}
+    end
+  end
+
    defp verfy_user(current_user_id, owner_user_id) do
     case Accounts.get_user(current_user_id)  do
       n when n.id == owner_user_id  -> {:ok, n}
