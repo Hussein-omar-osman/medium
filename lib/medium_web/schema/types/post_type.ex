3a96a8564d07620868898b6ab7c3cd14ad92baec
Hussein Omar <husseinomar6190@gmail.com>
2023-01-24 15:34:05 +0300

    add post types

---
+++ lib/medium_web/schema/types/post_type.ex
@@ -0,0 +1,18 @@
+defmodule MediumWeb.Schema.Types.PostType do
+
+  use Absinthe.Schema.Notation
+
+  object :post_type do
+    field :id, :id
+    field :title, :string
+    field :content, :string
+    field :thumbnail, :string
+    field :user, :user_type
+  end
+
+  input_object :post_input_type do
+    field :title, non_null(:string)
+    field :content, non_null(:string)
+    field :thumbnail, :string
+  end
+end
--- lib/medium_web/schema/types/types.ex
+++ lib/medium_web/schema/types/types.ex
@@ -4,5 +4,6 @@ defmodule MediumWeb.Schema.Types do
 
   import_types(MediumWeb.Schema.Types.UserType)
   import_types(MediumWeb.Schema.Types.SessionType)
+  import_types(MediumWeb.Schema.Types.PostType)
 
 end
