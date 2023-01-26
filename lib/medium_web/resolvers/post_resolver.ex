758750cfe8884939189c1dbebd6c8613e8ad5315
Hussein Omar <husseinomar6190@gmail.com>
2023-01-26 13:32:05 +0300

    add: resolver to handle updating post

--- lib/medium_web/resolvers/post_resolver.ex
+++ lib/medium_web/resolvers/post_resolver.ex
@@ -1,5 +1,5 @@
 defmodule MediumWeb.Resolvers.PostResolver do
-  alias Medium.Posts
+  alias Medium.{Posts, Accounts}
   alias Medium.Posts.Post
   alias MediumWeb.Utils
 
@@ -21,6 +21,26 @@ defmodule MediumWeb.Resolvers.PostResolver do
 
   def user_posts(_, %{input: %{id: id}}, _) do
     {:ok, Posts.list_posts(id)}
+  end
+
+  def update_post(_, %{input: input}, %{context: %{current_user: current_user}}) do
+    with  {:ok, post}  <- Posts.get_post(input.id),
+          {:ok, _} <- verfy_user(current_user.id, post.user_id),
+          {:ok, %Post{} = post_result} <- Posts.update_post(post, Map.delete(input, :id)) do
+
+          {:ok, post_result}
+       else
+        {:error, :not_found} -> {:error, "Post not found"}
+        {:error, :not_allowed} -> {:error, "You are not allowed to update this post"}
+        {:error, %Ecto.Changeset{} = changeset} -> {:error, Utils.format_changeset_error(changeset)}
+    end
+  end
+
+  defp verfy_user(current_user_id, owner_user_id) do
+    case Accounts.get_user(current_user_id)  do
+      n when n.id == owner_user_id  -> {:ok, n}
+      n when n.id != owner_user_id  -> {:error, :not_allowed}
+    end
   end
 
 end
