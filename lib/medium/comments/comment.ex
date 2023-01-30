3b4b8141f8f414a85d930351bc2c1bf43f15edba
Hussein Omar <husseinomar6190@gmail.com>
2023-01-30 12:18:04 +0300

    add: comments Ecto.schema

---
+++ lib/medium/comments/comment.ex
@@ -0,0 +1,25 @@
+defmodule Medium.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Medium.Accounts.User
+  alias Medium.Posts.Post
+
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "comments" do
+    field :content, :string
+    belongs_to :user, User, type: :binary_id
+    belongs_to :post, Post, type: :binary_id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content, :user_id, :post_id])
+    |> validate_required([:content, :user_id, :post_id])
+    |> validate_length(:title, min: 1,)
+  end
+end
