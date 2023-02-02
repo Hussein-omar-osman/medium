338eccb88eae3156f4535faf1e333b9601c19daa
Husssein Omar <husseinomar6190@gmail.com>
2023-02-02 15:20:23 +0300

    add: bookmark schema for db

---
+++ priv/repo/migrations/20230301121412_create_bookmarks.exs
@@ -0,0 +1,16 @@
+defmodule Medium.Repo.Migrations.CreateBookmarks do
+  use Ecto.Migration
+
+  def change do
+    create table(:bookmarks, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id)
+      add :post_id, references(:posts, on_delete: :delete_all, type: :binary_id)
+
+      timestamps()
+    end
+
+    create unique_index(:bookmarks, [:post_id, :user_id])
+
+  end
+end
