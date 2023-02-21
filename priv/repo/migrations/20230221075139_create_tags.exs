93e34cec87d2b3bd797e9dcf68b3f5c2109643d8
Husssein Omar <husseinomar6190@gmail.com>
2023-02-21 11:26:31 +0300

    add: db schema for tags & migration

---
+++ priv/repo/migrations/20230221075139_create_tags.exs
@@ -0,0 +1,14 @@
+defmodule Medium.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :tag, :string
+
+      timestamps()
+    end
+
+    create unique_index(:tags, [:tag])
+  end
+end
